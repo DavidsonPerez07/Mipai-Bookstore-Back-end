3ace58469d60cdc42c38470e7b479db13e872a4e
DavidsonPerezUdea <davidson.perez@udea.edu.co>
2024-04-01 12:04:47 -0500

    corregida la consulta findByEmail en User

--- mipai-bookstore-back/src/main/java/com/udea/edyl/EDyL/data/repository/UserRepository.java
+++ mipai-bookstore-back/src/main/java/com/udea/edyl/EDyL/data/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.udea.edyl.EDyL.data.repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.udea.edyl.EDyL.data.entity.User;
 
@@ -11,5 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u.email FROM User u")
     List<String> findEmailBy();
 
-    User findByEmail(String email);
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    User findByEmail(@Param("email") String email);
 }
